521b0fff93efd721b8f78530bb9d88957accec08
denchosu <98576912+denchosu@users.noreply.github.com>
2023-11-22 18:11:57 +0200

    Update lab5_2_Uhrimov.cpp

--- week 5/lab5_2_Uhrimov.cpp
+++ week 5/lab5_2_Uhrimov.cpp
@@ -9,39 +9,26 @@ double f(double x) {
 }
 
 int main() {
-    double a, b, h;
-
-    // Введення початку інтервалу (a)
-    cout << "Введіть початок інтервалу (a): ";
-    cin >> a;
-
-    // Введення кінця інтервалу (b)
-    cout << "Введіть кінець інтервалу (b): ";
-    cin >> b;
-
-    // Введення кроку (h)
-    cout << "Введіть крок (h): ";
+        int a, b, h;
+    double f;
+    cout << "Enter start and end values: \n";
+    cin >> a >> b;
+    cout << "Enter step h: \n";
     cin >> h;
-
-    if (h <= 0) {
-        cout << "Крок (h) має бути додатнім числом." << endl;
-        return 1; // Вихід з програми з помилкою
+    cout << "-----------------" << endl;
+    cout << "|---X---|---Y---|" << endl;
+    cout << "-----------------" << endl;
+    for (; a <= b; a += h)
+    {
+        if (cos(a) <= 0)
+        {
+            cout << "Out of range of permissible values." << endl;
+            continue;
+        }
+        f = (sqrt(cos(a))) + sin(a);
+        cout << "|---" << a << "---|---" << f << "---|" << endl;
+        cout << "-----------------" << endl;
     }
-
-    // Виведення заголовка таблиці
-    cout << "-----------------------------" << endl;
-    cout << ":       X       :       Y       :" << endl;
-    cout << "-----------------------------" << endl;
-
-    // Табулювання та виведення результатів
-    double x = a;
-    while (x <= b) {
-        double y = f(x);
-        cout << ": " << setw(13) << x << " : " << setw(13) << y << " :" << endl;
-        x += h;
-    }
-
-    cout << "-----------------------------" << endl;
 
     return 0;
 }
